2424d41d431111194c21fe483769cf3a64940605
Rahul Gupta <guptarahul70322@gmail.com>
2020-09-12 18:13:14 +0530

    added global theme

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -22,7 +22,7 @@ class TransactionList extends StatelessWidget {
                   ),
                   decoration: BoxDecoration(
                     border: Border.all(
-                      color: Colors.purple,
+                      color: Theme.of(context).primaryColor,
                       width: 2,
                     ),
                   ),
@@ -31,18 +31,17 @@ class TransactionList extends StatelessWidget {
                     style: TextStyle(
                       fontWeight: FontWeight.bold,
                       fontSize: 20,
-                      color: Colors.purple,
+                      color: Theme.of(context).primaryColor,
                     ),
                   ),
                 ),
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    Text(transactions[index].title,
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        )),
+                    Text(
+                      transactions[index].title,
+                      style: Theme.of(context).textTheme.bodyText1,
+                    ),
                     Text(
                       DateFormat.yMMMMd().format(transactions[index].date),
                       style: TextStyle(
